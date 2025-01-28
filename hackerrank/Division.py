dcf40f3fc0cc835105917d60f0f03a48ef979c63
SaiCharmika <saicharmika@gmail.com>
2025-01-28 11:00:58 +0530

    Create Division.py

---
+++ hackerrank/Division.py
@@ -0,0 +1,9 @@
+if __name__ == '__main__':
+    a = int(input())  # Read the first integer
+    b = int(input())  # Read the second integer
+
+    # Print the result of integer division
+    print(a // b)
+
+    # Print the result of float division
+    print(a / b)
